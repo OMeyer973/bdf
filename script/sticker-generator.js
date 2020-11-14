77f62a55182cc2c898dc20b4e3661d34349c729d
Olivier <olivier.meyer973@gmail.com>
2020-11-14 11:59:18 +0100

    fixed multi line sticker generator

--- script/sticker-generator.js
+++ script/sticker-generator.js
@@ -1,15 +1,18 @@
 var stickerText = document.getElementById("sticker-text")
 document.getElementById("input-text").addEventListener("input", applyInput)
 
-var customBeforeStyle = document.head.appendChild(document.createElement("style"));
-var customAfterStyle = document.head.appendChild(document.createElement("style"));
+var customTextStyle = document.head.appendChild(document.createElement("style"))
+var customBeforeStyle = document.head.appendChild(document.createElement("style"))
+var customAfterStyle = document.head.appendChild(document.createElement("style"))
 
 function applyInput() {
-  var inputText = this.value
+  const inputText = this.value
   console.log("inputText" + inputText)
   stickerText.innerHTML = inputText.replace(/\r?\n/g, '&#10;')
+  const numberLines =  inputText.split("\n").length;
+  console.log(numberLines);
+  customTextStyle.innerHTML = "#sticker-text {font-size: " + 4.45 * 2 / numberLines + "em;"
   customBeforeStyle.innerHTML = "#sticker-text:before {content: '" + inputText.replace(/\r?\n/g, '\\A ') + "';"
   customAfterStyle.innerHTML = "#sticker-text:after {content: '" + inputText.replace(/\r?\n/g, '\\A ') + "';"
-// #sticker-text:after {content: '" + inputText + "';}
 }
 

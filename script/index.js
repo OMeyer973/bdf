e4ac6dc2ed2fee4262737ba7172e929c70cf4a1f
Olivier <olivier.meyer973@gmail.com>
2020-11-15 14:53:45 +0100

    added easter egg page link (it's hidden !)

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+// items descriptions
 var showItemDescriptionButtons = document.getElementsByClassName("show-item-description")
 for (var showItemDescriptionButton of showItemDescriptionButtons)
   showItemDescriptionButton.addEventListener('click', showItemDesctiption)
@@ -15,4 +16,18 @@ function showItemDesctiption()
     this.classList.remove("upside-down");
     itemDescription.classList.add("hidden")
   }
+}
+
+// sticker click
+var nbStickerClicks = 0;
+var sticker = document.getElementById("sticker")
+ sticker.addEventListener("click", onStickerClick);
+
+function onStickerClick() {
+   nbStickerClicks ++
+   setTimeout(function(){ nbStickerClicks -- }, 5000)
+   if (nbStickerClicks >= 10)
+    window.location.href = "sticker-generator.html";
+    sticker.classList.add("bigger")
+    setTimeout(function(){ sticker.classList.remove("bigger") }, 1)
 }
